9da5ea3a56e495eb894431876884fad6f895a33d
Joseph Wilk <joe@josephwilk.net>
2013-04-05 16:03:32 +0200

    wip: associations and inserts

--- test/fujin/integration/t_app.clj
+++ test/fujin/integration/t_app.clj
@@ -2,3 +2,21 @@
   (:require
     [midje.sweet :refer :all]
     [fujin.core :refer :all]))
+    
+
+(facts "insert"
+  (fact "deleting a user returns if the operation deleted something or not"
+    (deftable :address)
+    (-> delete users where {:id 1}))
+    
+  (fact "assocations are used to insert multiple rows"
+    (deftable :address
+      (belongs-to :users))
+
+    (deftable :users
+      (has-one :address))
+
+    (-> insert users {:name "josephwilk" :address {:postcode "1234"}})
+
+    (first (users limit 1)) => (contains {:name "josephwilk"})
+    (first (address limit 1)) => (contains {:postcode "1234"})))
