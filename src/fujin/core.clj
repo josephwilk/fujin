69faf68006d6c9cbcace1865c23b5ac5c0bb2662
josephwilk <joe@josephwilk.net>
2013-04-15 19:19:28 +0200

    Experimentation

--- src/fujin/core.clj
+++ src/fujin/core.clj
@@ -1,1 +1,30 @@
 (ns fujin.core)
+
+(defmacro deftable [entity & [associations]]
+  (let [table-name (name entity)]
+    `(defn ~(symbol table-name) [& [command#]]
+       (let [i-am-a-select# (nil? command#)
+             command# (or command# select)]
+         (if i-am-a-select#
+           (command# ~table-name)
+           (fn [attributes#] (command# ~table-name attributes#)))))))
+
+(defn where [command]
+  :id)
+
+(defn values [command attributes]
+  (command attributes))
+
+(defn select [table] (format "SELECT * FROM %s" table))
+
+(defn delete [table attributes]
+  (format "DELETE FROM %s WHERE %s" table attributes))
+
+(defn insert [table attributes]
+  (format "INSERT INTO %s VALUES [%s]" table attributes))
+
+(defn limit [])
+
+(defn belongs-to [entity])
+
+(defn has-one [entity])
--- test/fujin/integration/t_app.clj
+++ test/fujin/integration/t_app.clj
@@ -2,13 +2,17 @@
   (:require
     [midje.sweet :refer :all]
     [fujin.core :refer :all]))
-    
+
+(fact "select"
+  (deftable :users)
+  (-> insert users (values {:name "josephwilk"}))
+  (users) => [{:name "josephwilk"}])
 
 (facts "insert"
   (fact "deleting a user returns if the operation deleted something or not"
-    (deftable :address)
-    (-> delete users where {:id 1}))
-    
+    (deftable :users)
+    (-> delete users where {:id 1}) => 1)
+
   (fact "assocations are used to insert multiple rows"
     (deftable :address
       (belongs-to :users))
@@ -16,7 +20,7 @@
     (deftable :users
       (has-one :address))
 
-    (-> insert users {:name "josephwilk" :address {:postcode "1234"}})
+    (-> insert users (values {:name "josephwilk" :address {:postcode "1234"}}))
 
-    (first (users limit 1)) => (contains {:name "josephwilk"})
-    (first (address limit 1)) => (contains {:postcode "1234"})))
+    (users) => (contains {:name "josephwilk"})
+    (address) => (contains {:postcode "1234"})))
